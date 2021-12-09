7dbe255f844b82f6a9ce3fe3073b44095472171e
Stefanos <54231658+StefanosINC@users.noreply.github.com>
2021-12-09 10:57:51 -0800

    push
    push

---
+++ src/main/java/com/gcu/SeniorCapstoneApplication.java
@@ -0,0 +1,17 @@
+package com.gcu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.annotation.ComponentScan;
+
+@ComponentScan("com.gcu")
+@SpringBootApplication
+public class SeniorCapstoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeniorCapstoneApplication.class, args);
+	}
+
+
+}
